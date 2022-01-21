51b99f6c545651248cc31d97524621c8810baff8
Enrico De Bon <enricodebon@gmail.com>
2022-01-21 16:24:45 +0100

    Added some more open5gs config, even if it is not working ATM
    Signed-off-by: Enrico De Bon <enricodebon@gmail.com>

--- dockerImages/Open5GS/startup.sh
+++
@@ -1,7 +0,0 @@
-export IP_ADDR=$(awk 'END{print $1}' /etc/hosts)
-echo
-echo "AMF IP Address: ${IP_ADDR}"
-echo
-cp /etc/open5gs/amf.template.yaml /etc/open5gs/amf.yaml
-sed -i "s/IPADDR/${IP_ADDR}/g" /etc/open5gs/amf.yaml
-systemctl restart open5gs-amfd
