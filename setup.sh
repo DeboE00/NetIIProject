8754e4601071698ac80248440dda6eb803cdd205
Enrico De Bon <enricodebon@gmail.com>
2022-01-25 13:53:21 +0100

    Updated docker images
    / Updated Open5GS images to use the latest Ubuntu 20.04 as base (instaed of the older 18.04)
    / Make UERANSIM compile a specific version (to avoid annoying bugs)
    
    Signed-off-by: Enrico De Bon <enricodebon@gmail.com>

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 cd UERANSIM/
 git clone https://github.com/open5gs/open5gs.git
 cd open5gs/docker
-USER=open5gs DIST=ubuntu TAG=bionic BRANCH=v2.4.3 docker-compose build
+USER=open5gs DIST=ubuntu TAG=focal BRANCH=v2.4.3 docker-compose build
 # Create the UERANSIM image
 cd ../../../dockerImages/UERANSIM
 docker build -t registry.enricodebon.com:5000/university/networking-ii-2022-2021/project:ueransim .
