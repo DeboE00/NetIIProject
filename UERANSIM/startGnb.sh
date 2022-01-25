cbad89e5c691cd92267567584101d0c1156bac85
Enrico De Bon <enricodebon@gmail.com>
2022-01-25 11:34:05 +0100

    Fixed some dependencies
    But the container layouts must change, as we used the 4G network instaed of the 5G one... Whoops
    
    Signed-off-by: Enrico De Bon <enricodebon@gmail.com>

--- UERANSIM/startGnb.sh
+++ UERANSIM/startGnb.sh
@@ -1,7 +1,13 @@
+#!/bin/sh
+export AMF_STARTUP_TIME=20
 export IP_ADDR=$(awk 'END{print $1}' /etc/hosts)
 echo
 echo "gNB IP Address: ${IP_ADDR}"
 echo
+
+echo "Waiting for " ${AMF_STARTUP_TIME} "s for the AMF to be ready..."
+sleep ${AMF_STARTUP_TIME}
+
 cp /UERANSIM/custom-gnb.yaml /UERANSIM/custom-gnb.final.yaml
 sed -i "s/IPADDR/${IP_ADDR}/g" /UERANSIM/custom-gnb.final.yaml
 ./nr-gnb -c custom-gnb.final.yaml
--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Create the open5gs image
 cd UERANSIM/
 git clone https://github.com/open5gs/open5gs.git
 cd open5gs/docker
 USER=open5gs DIST=ubuntu TAG=bionic BRANCH=v2.4.3 docker-compose build
+# Create the UERANSIM image
+cd ../../../dockerImages/UERANSIM
+docker build -t registry.enricodebon.com:5000/university/networking-ii-2022-2021/project:ueransim .
